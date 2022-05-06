147da719d3cf0fb650bf4aab7ce17c1838387b42
Lars Trieloff <lars@trieloff.net>
2022-05-06 17:32:14 +0200

    ci(release): tag releases in Coralogix
    This is using the new `semantic-release-coralogix` plugin, which will tag
    each release in Coralogix, so that the cause of errors becomes more obvious.
    The required credentials have already been set up in the CircleCI context
    and the plugin has already been enabled for `helix-rum-collector` see
    https://app.circleci.com/pipelines/github/adobe/helix-rum-collector/375/workflows/337c8ebd-ebd1-4618-80ab-6f91392b549b/jobs/597

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -10,6 +10,15 @@ module.exports = {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
+    ['@adobe/semantic-release-coralogix', {
+      iconUrl: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/ship_1f6a2.png',
+      applications: [
+        'fastly' // "but, we aren't even deploying this to Fastly, I hear you say." That is correct. The Coralogix
+        // application filter for a tag will only show our releases when the Fastly filter is enabled. As
+        // there is no application logging within this project, errors will only become visible in Fastly,
+        // hence the application setting.
+      ]
+    }],
     ["@semantic-release/github", {}]
   ],
   branches: ['main'],
