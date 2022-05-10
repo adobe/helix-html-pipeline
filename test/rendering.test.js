529facf416a07e8a5050ec1a614eec23f9249efc
Tobias Bocanegra <tripod@adobe.com>
2022-05-10 15:34:11 +0200

    fix: pretty-print html
    BREAKING CHANGE: inter-element whitespace changes

--- test/rendering.test.js
+++ test/rendering.test.js
@@ -402,7 +402,7 @@ describe('Rendering', () => {
         .headers('metadata.json', 'x-amz-meta-x-source-last-modified', 'Wed, 12 Oct 2022 15:33:01 GMT');
       const { status, body, headers } = await render(new URL('https://helix-pipeline.com/blog/one-section'), '.plain');
       assert.strictEqual(status, 200);
-      assert.match(body, /<div class="test"><h1 id="hello">Hello<\/h1><p>This is the first section.<\/p><\/div>/);
+      assert.match(body, /<div class="test">\s*<h1 id="hello">Hello<\/h1>\s*<p>This is the first section.<\/p>\s*<\/div>/);
       assert.deepStrictEqual(Object.fromEntries(headers.entries()), {
         'content-type': 'text/html; charset=utf-8',
         'x-surrogate-key': '0j8f6rmY3lU5kgOE',
