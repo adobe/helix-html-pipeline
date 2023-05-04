5303c174830755f07019179cf9b53c198f3cf3e1
Tobias Bocanegra <tripod@adobe.com>
2023-05-04 09:49:00 +0200

    fix: use SameSite=None for auth cookie (#305)
    fixes #304

--- test/utils/auth-cookie.test.js
+++ test/utils/auth-cookie.test.js
@@ -16,24 +16,24 @@ import { clearAuthCookie, getAuthCookie, setAuthCookie } from '../../src/utils/a
 
 describe('Auth Cookie Test', () => {
   it('clears the auth cookie', () => {
-    assert.strictEqual(clearAuthCookie(), 'hlx-auth-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Lax');
+    assert.strictEqual(clearAuthCookie(), 'hlx-auth-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=None');
   });
 
   it('clears the auth cookie (secure)', () => {
-    assert.strictEqual(clearAuthCookie(true), 'hlx-auth-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Lax');
+    assert.strictEqual(clearAuthCookie(true), 'hlx-auth-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=None');
   });
 
   it('sets the auth cookie', () => {
     assert.strictEqual(
       setAuthCookie('1234'),
-      'hlx-auth-token=1234; Path=/; HttpOnly; SameSite=Lax',
+      'hlx-auth-token=1234; Path=/; HttpOnly; SameSite=None',
     );
   });
 
   it('sets the auth cookie (secure)', () => {
     assert.strictEqual(
       setAuthCookie('1234', true),
-      'hlx-auth-token=1234; Path=/; HttpOnly; Secure; SameSite=Lax',
+      'hlx-auth-token=1234; Path=/; HttpOnly; Secure; SameSite=None',
     );
   });
 
